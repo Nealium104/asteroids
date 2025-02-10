8ff6a14bb5bc0ab7a8e4f90c6d2bbeb0534dcf4d
Neal Powers <neal.grindstaff@gmail.com>
2025-02-09 21:02:30 -0500

    stopping point

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = time.tick(60) / 1000
